fa55d1a20232c4a1647b1da10fe89d4f10c02363
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-31 11:35:56 +0100

    answers from lesson 31.01.22

--- script.js
+++ script.js
@@ -9,6 +9,54 @@ cat.complain();
 
 // Question 2.
 
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// ****************************
+
+
+// Question 3
+
+heading.style.fontSize = "2em";
+console.log(heading);
+
+// ****************************
+
+
+// Question 4
+
+heading.classList.add("subheading");
+console.log(heading);
+
+// ****************************
+
+
+// Question 5
+
+const paragraphs = document.querySelectorAll("p");
+console.log(paragraphs);
+
+for (let i = 0; i < paragraphs.length; i++) {
+    console.log(paragraphs[i]);
+    paragraphs[i].style.color = "red";
+}
+
+// ****************************
+
+
+// Question 6
+
+const resultContainer = document.querySelector(".results");
+console.log(resultContainer.style);
+
+resultContainer.innerHTML = "<p>New paragraph</p>";
+
+resultContainer.style.backgroundColor = "yellow";
+
+// ****************************
+
+
+// Question 7
 
 const cats = [
     {
@@ -23,3 +71,37 @@ const cats = [
         age: 21
     }
 ];
+
+function catsName(list) {
+    for (let i = 0; i < list.length; i++) {
+        console.log("The cats name is: " + list[i].name);
+    }
+}
+
+catsName(cats);
+
+// ****************************
+
+
+// Question 8
+
+function createCats(cats) {
+    let myCatItems = "";
+    for (let i = 0; i < cats.length; i++) {
+        myCatItems += `
+        <div>
+        <h5>${cats[i].name}</h5>
+        <p>${cats[i].age ? cats[i].age : "Age unknown"}</p>
+        </div>
+        `;
+    }
+    return myCatItems;
+}
+
+const newCats = createCats(cats);
+console.log(newCats);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = newCats;
+
+// ****************************
